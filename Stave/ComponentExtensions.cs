a369a31398c538c16573c673e88270ffec6cf540
Rémi Morillon <rmorillon@freneticexperience.com>
2017-11-09 13:35:36 +0100

    Optimize ChildrenQueue and ParentQueue extensions

--- Stave/ComponentExtensions.cs
+++ Stave/ComponentExtensions.cs
@@ -6,54 +6,26 @@ namespace Stave
 {
     static public class ComponentExtensions
     {
-        static public IEnumerable ChildrenQueue<TBase>(this IComponent component)
-            where TBase : class, IComponent<TBase>
-        {
-            IEnumerable<TBase> children = Enumerable.Empty<TBase>();
-            foreach (TBase child in component.Components)
-            {
-                yield return child;
-                children = children.Concat(child.ChildrenQueue());
-            }
-
-            foreach (TBase child in children)
-                yield return child;
-        }
-
         static public IEnumerable<TBase> ChildrenQueue<TBase>(this IComponent<TBase> component)
             where TBase : class, IComponent<TBase>
         {
-            IEnumerable<TBase> children = Enumerable.Empty<TBase>();
             foreach (TBase child in component.Components)
-            {
                 yield return child;
-                children = children.Concat(child.ChildrenQueue());
-            }
 
-            foreach (TBase child in children)
+            foreach (TBase child in component.Components.SelectMany(x => x.ChildrenQueue()))
                 yield return child;
         }
 
         static public IEnumerable ParentQueue(this IComponent component)
         {
-            if (component.Parent == null)
-                yield break;
-
-            yield return component.Parent;
-
-            foreach (object parent in component.Parent.ParentQueue())
+            for (IComponent parent = component.Parent; parent != null; parent = parent.Parent)
                 yield return parent;
         }
 
         static public IEnumerable<TBase> ParentQueue<TBase>(this IComponent<TBase> component)
             where TBase : class, IComponent<TBase>
         {
-            if (component.Parent == null)
-                yield break;
-
-            yield return component.Parent;
-
-            foreach (TBase parent in component.Parent.ParentQueue())
+            for (TBase parent = component.Parent; parent != null; parent = parent.Parent)
                 yield return parent;
         }
 
