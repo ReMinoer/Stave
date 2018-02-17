cab95167aca700fe864796b118fb1c9e8d5c503d
Rémi Morillon <rmorillon@freneticexperience.com>
2018-02-17 10:03:34 +0100

    Update Diese  and use new Tree and Sequence utilitaries

--- Stave/ComponentExtensions.cs
+++ Stave/ComponentExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using Diese.Collections;
 
 namespace Stave
 {
@@ -9,37 +9,25 @@ namespace Stave
         static public IEnumerable<TBase> ChildrenQueue<TBase>(this IComponent<TBase> component)
             where TBase : class, IComponent<TBase>
         {
-            foreach (TBase child in component.Components)
-                yield return child;
-
-            foreach (TBase child in component.Components.SelectMany(x => x.ChildrenQueue()))
-                yield return child;
+            return Tree.BreadthFirstExclusive(component, x => x.Components);
         }
 
         static public IEnumerable ParentQueue(this IComponent component)
         {
-            for (IComponent parent = component.Parent; parent != null; parent = parent.Parent)
-                yield return parent;
+            return Sequence.AggregateExclusive(component, x => x.Parent);
         }
 
         static public IEnumerable<TBase> ParentQueue<TBase>(this IComponent<TBase> component)
             where TBase : class, IComponent<TBase>
         {
-            for (TBase parent = component.Parent; parent != null; parent = parent.Parent)
-                yield return parent;
+            return Sequence.AggregateExclusive(component, x => x.Parent);
         }
 
         static public IEnumerable<TContainer> ParentQueue<TBase, TContainer>(this IComponent<TBase, TContainer> component)
             where TBase : class, IComponent<TBase, TContainer>
             where TContainer : class, TBase, IContainer<TBase, TContainer>
         {
-            if (component.Parent == null)
-                yield break;
-
-            yield return component.Parent;
-
-            foreach (TContainer parent in component.Parent.ParentQueue())
-                yield return parent;
+            return Sequence.AggregateExclusive(component, x => x.Parent);
         }
     }
 }
